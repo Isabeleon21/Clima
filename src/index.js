ca7d1856655e52acdc4db24584a2136c515704bd
Isabeleon21 <isabella.eon@gmail.com>
2024-09-12 10:18:10 +0100

    More weather data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,43 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let tempElement = document.querySelector("#temp-unit");
   let cityElement = document.querySelector("#city");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let date = new Date(response.data.time * 1000);
+
+  console.log(response.data);
+
   cityElement.innerHTML = response.data.city;
   tempElement.innerHTML = Math.round(temperature);
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  dateElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
